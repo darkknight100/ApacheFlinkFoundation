470c4feaa02491aaeb9d696d9ba2209d6094652f
vaibhavgour <vaibhav.gour@razorpay.com>
2019-11-26 18:08:29 +0530

    Renamed the main class to StreamingJob

--- src/main/scala/StreamingJob.scala
+++ src/main/scala/StreamingJob.scala
@@ -2,11 +2,11 @@ import com.typesafe.scalalogging.Logger
 import configs.FlinkApplicationConfig
 import org.apache.flink.streaming.api.scala._
 
-object StreamingApp extends App{
+object StreamingJob extends App{
 
   override def main(args: Array[String]): Unit = super.main(args)
 
-  val logger = Logger(StreamingApp.getClass)
+  val logger = Logger(StreamingJob.getClass)
 
   val streamExecutionEnvironment: StreamExecutionEnvironment  = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(FlinkApplicationConfig.getConfig())
 
